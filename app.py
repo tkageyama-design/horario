dd8e1076801e71b964bab54cc6029a49287b3924
tkageyama-design <tkageyama01@gmail.com>
2026-03-05 17:28:10 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,65 +9,80 @@ historico = []
 @app.route("/", methods=["GET","POST"])
 def index():
 
-    saida = None
-    horas_trabalhadas = None
-    horas_restantes = None
+    saida_prevista=None
+    horas_trabalhadas=None
+    horas_restantes=None
+    horas_extras=None
 
-    entrada = ""
-    saida_almoco = ""
-    retorno_almoco = ""
+    entrada=""
+    saida_almoco=""
+    retorno_almoco=""
+    saida_real=""
 
-    if request.method == "POST":
+    if request.method=="POST":
 
-        entrada = request.form["entrada"]
-        saida_almoco = request.form["saida_almoco"]
-        retorno_almoco = request.form["retorno_almoco"]
+        entrada=request.form["entrada"]
+        saida_almoco=request.form["saida_almoco"]
+        retorno_almoco=request.form["retorno_almoco"]
+        saida_real=request.form.get("saida_real","")
 
-        fmt = "%H:%M"
+        fmt="%H:%M"
 
-        entrada_dt = datetime.strptime(entrada, fmt)
-        saida_almoco_dt = datetime.strptime(saida_almoco, fmt)
-        retorno_almoco_dt = datetime.strptime(retorno_almoco, fmt)
+        entrada_dt=datetime.strptime(entrada,fmt)
+        saida_almoco_dt=datetime.strptime(saida_almoco,fmt)
+        retorno_almoco_dt=datetime.strptime(retorno_almoco,fmt)
 
-        jornada = timedelta(hours=8)
+        jornada=timedelta(hours=8)
 
-        tempo_trabalhado = saida_almoco_dt - entrada_dt
-        tempo_almoco = retorno_almoco_dt - saida_almoco_dt
+        tempo_trabalhado=saida_almoco_dt-entrada_dt
+        horas_restantes_td=jornada-tempo_trabalhado
 
-        horas_restantes_td = jornada - tempo_trabalhado
+        saida_prevista_dt=retorno_almoco_dt+horas_restantes_td
+        saida_prevista=saida_prevista_dt.strftime("%H:%M")
 
-        saida_final = retorno_almoco_dt + horas_restantes_td
+        horas_trabalhadas=str(tempo_trabalhado)[:-3]
+        horas_restantes=str(horas_restantes_td)[:-3]
 
-        saida = saida_final.strftime("%H:%M")
+        if saida_real!="":
 
-        horas_trabalhadas = str(tempo_trabalhado)[:-3]
-        horas_restantes = str(horas_restantes_td)[:-3]
+            saida_real_dt=datetime.strptime(saida_real,fmt)
+
+            extras=saida_real_dt-saida_prevista_dt
+
+            if extras.total_seconds()>0:
+                horas_extras=str(extras)[:-3]
+            else:
+                horas_extras="00:00"
 
         historico.insert(0,{
             "entrada":entrada,
             "saida_almoco":saida_almoco,
             "retorno_almoco":retorno_almoco,
-            "saida":saida
+            "saida_prevista":saida_prevista,
+            "saida_real":saida_real,
+            "extras":horas_extras
         })
 
     return render_template(
         "index.html",
-        saida=saida,
-        horas_trabalhadas=horas_trabalhadas,
-        horas_restantes=horas_restantes,
-        historico=historico,
         entrada=entrada,
         saida_almoco=saida_almoco,
-        retorno_almoco=retorno_almoco
+        retorno_almoco=retorno_almoco,
+        saida_real=saida_real,
+        saida_prevista=saida_prevista,
+        horas_trabalhadas=horas_trabalhadas,
+        horas_restantes=horas_restantes,
+        horas_extras=horas_extras,
+        historico=historico
     )
-
 
 @app.route("/limpar")
 def limpar():
+
     historico.clear()
     return redirect("/")
 
+if __name__=="__main__":
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    port=int(os.environ.get("PORT",5000))
     app.run(host="0.0.0.0",port=port)
